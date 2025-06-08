1386253665e35803782642a10c283bf6f15a1e4a
RaulFer <88861872+kio145@users.noreply.github.com>
2025-06-08 15:00:08 -0400

    prueba migraciones

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -5,19 +5,16 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Database\Seeders\UserSeeder;
-use Database\Seeders\AdministradorSeeder;
-use Database\Seeders\CajeroSeeder;
 use Database\Seeders\CompetenciaSeeder;
-use Database\Seeders\CompetidorSeeder;
-use Database\Seeders\TutorSeeder;
 use Database\Seeders\BoletaPagoSeeder;
 use Database\Seeders\FechaSeeder;
 use Database\Seeders\InscripcionSeeder;
 use Database\Seeders\RequisitoCompetenciaSeeder;
-use Database\Seeders\CompetidorTutorSeeder;
-use Database\Seeders\ValidacionTutorSeeder;
+
+use Illuminate\Support\Facades\DB;
 
 
+DB::table('competencia')->truncate();
 class DatabaseSeeder extends Seeder
 {
     /**
